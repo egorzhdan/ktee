9f805ea7bf301d243e69d6e95838988274d4caa2
Egor Zhdan <egor.zhdan@gmail.com>
2018-02-15 23:20:48 +0300

    Add ignore interrupt feature

--- src/main/com/egorzh/ktee/Main.kt
+++ src/main/com/egorzh/ktee/Main.kt
@@ -1,8 +1,14 @@
 package com.egorzh.ktee
 
+import sun.misc.Signal
+
 fun main(args: Array<String>) {
     val arguments = Arguments(args)
     val output = Output(arguments)
+
+    if (arguments.ignoreInterrupt) {
+        Signal.handle(Signal("INT")) { }
+    }
 
     System.`in`.reader().forEachLine { output.println(it) }
 }
